07984f104f49765f49fa88bd204fd6e32722fdf1
TaN1k <nikitat612006@gmail,com>
2023-04-09 01:05:31 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   useEffect(() => {
     tg.ready()
   }, [])
-
-    const OnClick =  () => {
-        fetch(`https://api.telegram.org/bot5936180733:AAGjA1lGT2kktrghMMjodGfg1YNSPN2PLVA/sendMessage?chat_id=${tg.initDataUnsafe.user.id}&text=${document.getElementById("test").value}`)
-    }
   return (
     <div className="App">
       <div className="Test"></div>
