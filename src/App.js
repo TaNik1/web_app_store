f46b8539ec6e1f3e860c726a6318486e1d80bfcf
TaN1k <nikitat612006@gmail,com>
2023-04-09 00:59:09 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ function App() {
 
     const OnClick =  () => {
         fetch(`https://api.telegram.org/bot5936180733:AAGjA1lGT2kktrghMMjodGfg1YNSPN2PLVA/sendMessage?chat_id=${tg.initDataUnsafe.user.id}&text=${document.getElementById("test").value}`)
-        ///tg.sendData(document.getElementById("test").value)
     }
   return (
     <div className="App">
-      <input type={"text"} size={40} id={"test"}/>
-        <button onClick={OnClick}>Отправить</button>
+      <div className="Test"></div>
     </div>
   );
 }
