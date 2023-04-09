7a11120dbd04ce82dbfcec2c042f4d5dcd92d57b
TaN1k <nikitat612006@gmail,com>
2023-04-09 16:52:04 +0500

    commit

---
+++ scr/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from "react";
+import './App.css';
+const tg = window.Telegram.WebApp;
+function App() {
+
+    useEffect(() => {
+        tg.ready()
+    }, [])
+    return (
+        <div className="App">
+            <div className="Test">Помогите</div>
+        </div>
+    );
+}
+
+export default App;
