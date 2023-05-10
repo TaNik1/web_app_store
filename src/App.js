60b56c2ff2290c5589b140c47e88e0c5c9bd1e08
TaN1k <nikitat612006@gmail,com>
2023-05-10 22:20:20 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
+import React, {useEffect} from "react";
 import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
-import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
-
+import Category from "./Category/Category";
+import CategoryList from "./CategoryList/CategoryList";
+const tg = window.Telegram.WebApp;
 function App() {
-    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
-        tg.ready();
+        tg.ready()
     }, [])
-
     return (
         <div className="App">
-            <Header />
-            <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
-            </Routes>
+            <CategoryList />
         </div>
     );
 }
